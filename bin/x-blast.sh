605a1416e695992b8f2b1036cca5f55e9eb4c653
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2013-05-13 14:44:11 +0200

    Removed the '-Z' parameter from the wu-blastn command

--- bin/x-blast.sh
+++ bin/x-blast.sh
@@ -17,7 +17,7 @@ blastn -db $2/db -query $3 -outfmt '6 qseqid sseqid evalue score qgi bitscore le
 ;;
 
 'wu-blast')
-wu-blastn $2/db $3 -mformat=2 -Z=$dbSizes W=7 M=5 N=-4 Q=20 R=10 -e 0.00001 -cpus 1 -filter=seg -lcfilter
+wu-blastn $2/db $3 -mformat=2 W=7 M=5 N=-4 Q=20 R=10 -e 0.00001 -cpus 1 -filter=seg -lcfilter
 ;;
 
 *) echo "Not a valid BLAST strategy: $1"; exit 1
