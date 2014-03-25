8bd5b931e06fa4f5e0e352483079e2dd96d4e949
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-03-25 13:54:55 +0100

    Update cloud-boot.sh

--- cloud-boot.sh
+++ cloud-boot.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
+set -x 
 REPO='/glusterfs/users/tcoffee/piper-nf/'
+MAX=10
+count=0
+while [ $count -lt $MAX ]; do
+  count=$(( $count + 1 ))
+  ping -c1 index.docker.io >/dev/null  
+
+  [[ $? -eq 0 || $count -eq $MAX ]] && break
+  echo "No connection: sleeping for a while ... ($count)"
+  sleep $(( $count * 5 ))
+done
 
 # Load the PIPE-R image
 docker pull cbcrg/piper-nf
@@ -9,6 +20,8 @@ docker pull cbcrg/piper-nf
 wget -q -O - http://get.nextflow.io > nextflow && chmod +x nextflow
 ./nextflow -d -daemon.interface eth0  
 
-# Save this node IP address to a shared file 
+# save the environment for debugging 
+env | sort > .boot.env
 
+# Save this node IP address to a shared file 
 ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' >> $REPO/nodes.txt
