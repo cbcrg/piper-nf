42313d58268ed2891e5b94a4d39a1cc61fe02aee
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-08-30 16:17:41 +0200

    cloud boot file

--- cloud-boot.sh
+++ cloud-boot.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 set -e
 set -x 
-REPO='/glusterfs/users/tcoffee/piper-nf/'
-MAX=10
-count=0
-while [ $count -lt $MAX ]; do
-  count=$(( $count + 1 ))
-  ping -c1 index.docker.io >/dev/null  
 
-  [[ $? -eq 0 || $count -eq $MAX ]] && break
-  echo "No connection: sleeping for a while ... ($count)"
-  sleep $(( $count * 5 ))
-done
+#
+# Lauch docker and pull the container when DOCKER variable is defined
+#
+if [ -z "DOCKER" ]; then
+    MAX=10
+    count=0
+    while [ $count -lt $MAX ]; do
+      count=$(( $count + 1 ))
+      ping -c1 index.docker.io >/dev/null
 
-# Load the PIPE-R image
-docker pull cbcrg/piper-nf
+      [[ $? -eq 0 || $count -eq $MAX ]] && break
+      echo "No connection: sleeping for a while ... ($count)"
+      sleep $(( $count * 5 ))
+    done
 
-# Install NEXTFLOW and launch it 
-export NXF_VER=0.7.1
+    # pull the requested container
+    docker pull $DOCKER
+fi
+
+#
 wget -q -O - http://get.nextflow.io > nextflow && chmod +x nextflow
 ./nextflow -bg \
   -daemon.interface eth0 \
@@ -28,10 +32,10 @@ wget -q -O - http://get.nextflow.io > nextflow && chmod +x nextflow
   -daemon.tcp.networkTimeout 30s \
   -daemon.tcp.ackTimeout 30s \
   -daemon.join file:/glusterfs/users/tcoffee/piper-nf/cluster/
-  
+
 
 # save the environment for debugging 
 env | sort > .boot.env
 
-# Save this node IP address to a shared file 
+# Save this node IP address to a shared file
 ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' >> $REPO/nodes.txt
